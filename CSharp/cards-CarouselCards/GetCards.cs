4d9e4e8c70d9c3b8607c51ca6039b4ea7533d3d5
jlwkid244 <jasowang@umich.edu>
2017-07-25 15:56:34 -0700

    some more dialog updates; show receipt; allow multi item ordering; sum totals

--- CSharp/cards-CarouselCards/GetCards.cs
+++ CSharp/cards-CarouselCards/GetCards.cs
@@ -15,42 +15,42 @@ namespace CarouselCardsBot
             {
                 GetHeroCard(
                     "Azure Storage",
-                    "Offload the heavy lifting of data center management",
+                    "$4",
                     "Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.",
                     new CardImage(url: "https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png")),
                 GetHeroCard(
                     "Jason's golf putter",
-                    "worth your money",
+                    "$999",
                     "help you improve your putting skill",
                     new CardImage(url: "http://www.antiqueclubs.com/images4/Putters401/LeadtopPutter401.jpg")),
                 GetHeroCard(
                     "DocumentDB",
-                    "Blazing fast, planet-scale NoSQL",
+                    "$2.50",
                     "NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.",
                     new CardImage(url: "https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png")),
                 GetHeroCard(
                     "Azure Functions",
-                    "Process events with a serverless code architecture",
+                    "$1",
                     "An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.",
                     new CardImage(url: "https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png")),
                 GetHeroCard(
                     "Cognitive Services",
-                    "Build powerful intelligence into your applications to enable natural and contextual interactions",
+                    "$7",
                     "Enable natural and contextual interaction with tools that augment users' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.",
                     new CardImage(url: "https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png")),
             };
         }
 
-        private static Attachment GetHeroCard(string title, string subtitle, string text, CardImage cardImage)
+        private static Attachment GetHeroCard(string title, string price, string text, CardImage cardImage)
         {
-            var cardAction1 = new CardAction() { Type = ActionTypes.ImBack, Title = "1", Value = $" Order 1 {title}" };
-            var cardAction2 = new CardAction() { Type = ActionTypes.ImBack, Title = "2", Value = $"Order 2 {title}" };
-            var cardAction3 = new CardAction() { Type = ActionTypes.ImBack, Title = "3", Value = $"Order 3 {title}" };
+            var cardAction1 = new CardAction() { Type = ActionTypes.ImBack, Title = "1", Value = $"1 {title} @ {price}" };
+            var cardAction2 = new CardAction() { Type = ActionTypes.ImBack, Title = "2", Value = $"2 {title} @ {price}" };
+            var cardAction3 = new CardAction() { Type = ActionTypes.ImBack, Title = "3", Value = $"3 {title} @ {price}" };
 
             var heroCard = new HeroCard
             {
                 Title = title,
-                Subtitle = subtitle,
+                Subtitle = price,
                 Text = text,
                 Images = new List<CardImage>() { cardImage },
                 Buttons = new List<CardAction>() { cardAction1, cardAction2, cardAction3 },
--- CSharp/cards-CarouselCards/MenuDialog.cs
+++ CSharp/cards-CarouselCards/MenuDialog.cs
@@ -9,8 +9,11 @@
     [Serializable]
     public class MenuDialog : IDialog<object>
     {
+        public List<string> order;
         public async Task StartAsync(IDialogContext context)
         {
+            order = new List<string>();
+
             await context.PostAsync("Okay, here is our menu. Let me know when you ready.");
 
             Activity replyMenu = (Activity)context.MakeMessage();
@@ -24,10 +27,53 @@
 
         public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
-            await context.PostAsync("Your order has been placed! Let me know if you want to order again!");
+            var newOrder = await result;
+            order.Add(newOrder.Text);
+            await context.PostAsync($"Ordering {newOrder.Text} each! Do you want anything else?");
 
-            // This will return us to RootDialog
-            context.Done(true);
+            context.Wait(this.TestFunction);
+        }
+
+        public async Task TestFunction(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var response = await result;
+            if(response.Text.ToLower().Contains("$"))
+            {
+                var newOrder = await result;
+                order.Add(newOrder.Text);
+                await context.PostAsync($"Ordering {newOrder.Text} each! Do you want anything else?");
+
+                context.Wait(this.TestFunction);
+            }
+            else if(response.Text.ToLower().Contains("y"))
+            {
+                await context.PostAsync("Here is our menu again. Let me know what you want.");
+
+                Activity replyMenu = (Activity)context.MakeMessage();
+                replyMenu.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                replyMenu.Attachments = GetCards.GetCardsAttachments();
+                await context.PostAsync(replyMenu);
+
+                context.Wait(this.MessageReceivedAsync);
+            }
+            else
+            {
+                await context.PostAsync("Thanks for ordering! Here's your order summary. See you next time!");
+
+                int total = 0;
+                foreach(var item in order)
+                {
+                    int indexDollarSign = item.IndexOf("$");
+                    int indexFirstSpace = item.IndexOf(" ");
+                    int quantity = Convert.ToInt32(item.Substring(0, indexFirstSpace));
+                    total += quantity * Convert.ToInt32(item.Substring(indexDollarSign + 1));
+                    await context.PostAsync($"{item}");
+                }
+                await context.PostAsync($"Total: ${total}");
+
+                // This will return us to RootDialog
+                context.Done(true);
+            }
         }
     }
 }
